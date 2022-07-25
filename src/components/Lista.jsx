716b63f535915cd5348722f9fe07f0546542a609
Domyprefabrykowanemk <domyprefabrykowanemk@gmail.com>
2022-07-25 09:33:08 +0200

    123

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Iframe from "react-iframe";
 
 let baseURL =
-  "https://raw.githubusercontent.com/galeriapodsloncem/3dstrona/master/babylonjs-app/public/assets/";
+  "https://raw.githubusercontent.com/cholujdesign/Strona-CHD/main/src/components/renders/";
 const Lista = [
-   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294637633_5459258220797419_1771762644444398829_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=RAa-VyDsEi8AX-U97ke&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9vnNdz6XHEF1fzNlcxF_MTROJQTrHKrK2IyoaaODQJxA&oe=62DC3A0D", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294550007_5459259637463944_5492899516307500657_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q3Yo07u4hSsAX-W2LWd&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9S5552-B4zmLKxCfY9mmYk_W45BNAtBTqdchlw4QghXg&oe=62DBD85E",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294606072_5459258234130751_3712938801781668456_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=gzp5LM8aVEsAX-3yy3f&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-DpNhvtSrRC6iaDLpkU3RimvYRHNZiCq0XRiR6Nhdxxw&oe=62DB3E2C",
+  {
+    img1: baseURL+"rakowicka1.webp",
+    img2: baseURL+"rakowicka2.webp",
+    img3: baseURL+"rakowicka3.webp",
     plname: "Rokowicka 15 ",
     pldescription:
       "Projekt, nadzór i realizacja",
@@ -38,12 +38,12 @@ const Lista = [
     ),
   },
   {
-    img1: "https://external.flcj1-1.fna.fbcdn.net/emg1/v/t13/13954578319453198672?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FndNZSfyNUcY%2Fhqdefault.jpg&fb_obo=1&utld=ytimg.com&stp=c0.5000x0.5000f_dst-jpg_flffffff_p480x250_q75&ccb=13-1&oh=00_AT98ZxACbRIXxQWa-kDMceWVTgiABFA26IUZW3zPzttoLg&oe=62D88647&_nc_sid=c504da", 
-    img2: "https://external.flcj1-1.fna.fbcdn.net/emg1/v/t13/13954578319453198672?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FndNZSfyNUcY%2Fhqdefault.jpg&fb_obo=1&utld=ytimg.com&stp=c0.5000x0.5000f_dst-jpg_flffffff_p480x250_q75&ccb=13-1&oh=00_AT98ZxACbRIXxQWa-kDMceWVTgiABFA26IUZW3zPzttoLg&oe=62D88647&_nc_sid=c504da",
-    img3: "https://external.flcj1-1.fna.fbcdn.net/emg1/v/t13/13954578319453198672?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FndNZSfyNUcY%2Fhqdefault.jpg&fb_obo=1&utld=ytimg.com&stp=c0.5000x0.5000f_dst-jpg_flffffff_p480x250_q75&ccb=13-1&oh=00_AT98ZxACbRIXxQWa-kDMceWVTgiABFA26IUZW3zPzttoLg&oe=62D88647&_nc_sid=c504da",
+    img1: baseURL+"wieliczka.webp",
+    img2: baseURL+"wieliczka.webp",
+    img3: baseURL+"wieliczka.webp", 
     plname: "Domy pod Wieliczka",
     pldescription: <a className="white-text" href="https://www.youtube.com/watch?v=ndNZSfyNUcY">Projekt, nadzór i realizacja, https://www.youtube.com/watch?v=ndNZSfyNUcY</a>
-      ,
+    ,
     endescription: "https://www.facebook.com/permalink.php?story_fbid=pfbid0R2iqCCaSZNzEuL1sp4gMSc9faChoUZXFVNCqV3ZsPufjHJP9U5MsS9unyZ7hdMEnl&id=321208277935798",
     lokalizacja: "Kraków",
     rodzaj: "komercyjne",
@@ -71,10 +71,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294585133_5459386624117912_6987432424179995654_n.jpg?stp=dst-jpg_p720x720&_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=8ap1RPD94u4AX-fl4bd&_nc_ht=scontent.flcj1-1.fna&oh=00_AT91SCjuIuiT4vpGzSDkxKf3GI22cfYyMk5v7rMPam3L5Q&oe=62DC722C", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294605323_5459385817451326_4056682290576631135_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=uRUjeoToXV8AX9jaH8S&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-Pzo7T96IE6nnYLVrY0PJK-do8hQ3CBDCqA7VD7CT78w&oe=62DAD375",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294677830_5459386664117908_6468770226415657470_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=KtN8CuaR0VkAX-6nfaC&tn=o5ey6nqbzOgQXIue&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-wozxP6B73lx-5ouO6TLIrweXi3hm0kG_DVJsGtzGsBA&oe=62DADA91",
-    plname: "Koncepcje",
+    img1: baseURL+"koncepcja1.webp",
+    img2: baseURL+"koncepcja2.webp",
+    img3: baseURL+"koncepcja3.webp",     plname: "Koncepcje",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -104,10 +103,10 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294857727_5459831080740133_392543274326459178_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=VXnh2zbV9hQAX9UvElq&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8c6W0PGXORw25qUSWNK20PSOLJbXUz3YZ-KurBB6fiIg&oe=62DBF9A6", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294811409_5459831220740119_2849604516966265515_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=rn05zbZmubMAX8SoImZ&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_opKtIt4JFQSjhD75RqP1_Ja1IdSwiHCoXBd3fEwEu5Q&oe=62DC6283",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294540494_5459831334073441_2713102603376993310_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=5OULXthq3MgAX9BDx8D&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9SAEmL063a4Z8ClRuYHOfDktEv-T24nHMil50hj75JAw&oe=62DC15F4",
-    plname: "Domy prefabrykowane dla Hosue Q",
+    img1: baseURL+"houseq1.webp",
+    img2: baseURL+"houseq2.webp",
+    img3: baseURL+"houseq3.webp", 
+        plname: "Domy prefabrykowane dla Hosue Q",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "https://www.facebook.com/CHDarchitekci/posts/pfbid02MpJLiFz6En4AvWNT7wEj4x3b4bA68uYPyZ68uv44FTDjxnhbn8BepWe2wZeHirxRl",
@@ -138,10 +137,10 @@ const Lista = [
   },
 
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294652500_5459802920742949_2003844211235350248_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=tamv7iWlF7UAX8XTMI_&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_URZe8BSDdbqFUwPg_PCXOmd00vVUEHBbq0R7HMyVgAg&oe=62DBAEDE", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294727867_5459796030743638_5421270123518175354_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=nG9Y0auo01QAX_ryAVu&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8EJQni4RzA2IxYNhhxoXcXpMe22tv1xo1rg4TE4d63SQ&oe=62DAE5DB",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294545825_5459802904076284_915908708253282124_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=JMTFROhOS_UAX9AtUq8&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9UXYSwnl7VktzCs7rAqnffmuEqk3YpoNQKWS8a7oeKnA&oe=62DBD342",
-    plname: "",
+    img1: baseURL+"turaszwoka1.webp",
+    img2: baseURL+"turaszowka2.webp",
+    img3: baseURL+"turaszwoak3.webp", 
+        plname: "",
     pldescription:
       "Projekt, nadzór i realizacja dla Hosue Q",
     endescription: "",
@@ -171,10 +170,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294513970_5459907027399205_4461154066782848590_n.jpg?stp=dst-jpg_p180x540&_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Q2hwhnNMoesAX9PUaYc&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9APtuCQQCmdoulBsLmhz5_CzAsc9ehRV9TvYRtlgyEXA&oe=62DBB30A", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294864534_5459907090732532_1422648611873246676_n.jpg?stp=dst-jpg_p180x540&_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=fC8gf2dGmPkAX89NBG4&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_QZMc2mY5seWzdAmsxiiF15m7zMJGcOd_cahkvdqNjgQ&oe=62DBFE6C",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294620072_5459907380732503_13702742715975653_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=H0iuyaXgzHQAX-4K1q-&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9gnv69tm2hokAvuVKcTm5MgPVQNWRdnxbNQMNlK7mcZA&oe=62DB4FD2",
-    plname: "Willa Złocień",
+    img1: baseURL+"zlocien1.webp",
+    img2: baseURL+"zlocien2.webp",
+    img3: baseURL+"zlocien3.webp",     plname: "Willa Złocień",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -203,7 +201,7 @@ const Lista = [
       </div>
     ),
   },
-    // {
+  // {
   //   img1: "", 
   //   img2: "",
   //   img3: "",
--- src/components/ListaNoncommercial.jsx
+++ src/components/ListaNoncommercial.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Iframe from "react-iframe";
 
 let baseURL =
-  "https://raw.githubusercontent.com/galeriapodsloncem/3dstrona/master/babylonjs-app/public/assets/";
+  "https://raw.githubusercontent.com/cholujdesign/Strona-CHD/main/src/components/renders/";
+
 const Lista = [
   {
-    img1:  "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294566281_5459333730789868_7717108332365000321_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=PX8CjkGDgI8AX9a7ty3&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8O7CR6klsZunZNnHd04Xgon7hEpPb5Q6n7DiaZXwGaEQ&oe=62DAEA20",
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294566281_5459333730789868_7717108332365000321_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=PX8CjkGDgI8AX9a7ty3&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8O7CR6klsZunZNnHd04Xgon7hEpPb5Q6n7DiaZXwGaEQ&oe=62DAEA20",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294566281_5459333730789868_7717108332365000321_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=PX8CjkGDgI8AX9a7ty3&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8O7CR6klsZunZNnHd04Xgon7hEpPb5Q6n7DiaZXwGaEQ&oe=62DAEA20",
-    plname: "Restauracja Pod Słońcem, Kawiarnia Gehanowska",
+    img1: baseURL+"gehanowska.webp",
+    img2: baseURL+"gehanowska.webp",
+    img3: baseURL+"gehanowska.webp",     plname: "Restauracja Pod Słońcem, Kawiarnia Gehanowska",
     pldescription:
       "Projekt, nadzór i realizacja Kawiarni Gehanowskiej oraz Restauracji Pod Sloncem w Rynku Glównym w Krakowie pow.ok 300m2 czas realizacji 1994r",
     endescription: "",
@@ -38,10 +38,10 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294639811_5459345190788722_5927667336144799376_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=ALjv3L_Wzj4AX_lR50j&_nc_ht=scontent.flcj1-1.fna&oh=00_AT90TOIiYsILcuH5s_JZivWguJ8EXyVtWYkncOG8TvWRrQ&oe=62DBB840", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294502390_5459344680788773_8801093857956179143_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=J6GzR4gA8-UAX-Wpg6h&_nc_ht=scontent.flcj1-1.fna&oh=00_AT93HNGQxuSGVJOQCfL-_HshPUruRvP7Fuv9bxIDZ6-hCg&oe=62DC80D1",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294610970_5459345167455391_7309609277264073278_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=MBUglOQAE4UAX8HAkxX&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-Lk8W7HwRblynSkxD590CZt3zFShCm3TNzo9cVW3qchg&oe=62DB409D",
-    plname: "Apartament na wynajem",
+    img1: baseURL+"rakowickamieszkanie31.webp",
+    img2: baseURL+"rakowickamieszkanie32.webp",
+    img3: baseURL+"rakowickamieszkanie33.webp", 
+        plname: "Apartament na wynajem",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -71,10 +71,10 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294657351_5459347600788481_6407934965184931662_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=w0lFAwg5V6MAX_MdDnf&tn=o5ey6nqbzOgQXIue&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-lliCnSfqQM-JsX6hVjnfbcTGDAiCHziIdAyr55YHByg&oe=62DB7961", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294821165_5459347617455146_7788888652052236026_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=jyKdzGXxWP8AX9QZFe6&_nc_oc=AQlN3EvWSk7j8HAJWl4_AxG0QysXkzVzlBu3MYbXVwrlYfDtLo5_REeet6MgOgkuAn4&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8k3iRljG2p7Cc6RBjkytakpduLZVpdfe8GT0z8Et6T_g&oe=62DBA250",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294620652_5459347624121812_6815755033450738232_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=bQ2GcPjzG-0AX9crqmt&tn=o5ey6nqbzOgQXIue&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_9_sdcLzViUcb1TQI0IbemxsxcVnDc_i75drqs3Inoeg&oe=62DBD639",
-    plname: "Apartament na wynajem",
+    img1: baseURL+"rakowickamieszkanie21.webp",
+    img2: baseURL+"rakowickamieszkanie22.webp",
+    img3: baseURL+"rakowickamieszkanie23.webp", 
+        plname: "Apartament na wynajem",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -104,9 +104,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294641347_5459349260788315_1887007953773178371_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=QZ7WdDGIcNUAX-SydAO&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-Y0d4F-24pk5IM--nGmufIPs0CjU0h8yLEPEzR8PeYmQ&oe=62DB6A89", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294580895_5459349277454980_7489487358793378712_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=7iE4j4-EG-EAX_f3XBE&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_XOgALMreCwzolBluOhbaQHtlAtRF_1Xmnt4AVe0kQtQ&oe=62DB6D3A",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294501506_5459349270788314_4487083802203228205_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=pxYqjnz1jqwAX_hJ1On&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8UOqzm6slnTgD0BoxqeyXatR0GfzzY6r15-uBsC_G0rw&oe=62DB1FCA",
+    img1: baseURL+"rakowickamieszkanie31.webp",
+    img2: baseURL+"rakowickamieszkanie32.webp",
+    img3: baseURL+"rakowickamieszkanie33.webp", 
     plname: "Apartament na wynajem",
     pldescription:
       "Projekt, nadzór i realizacja",
@@ -137,9 +137,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294849381_5459352950787946_5763242396441239763_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=OHS_golGXDgAX8WcMji&_nc_ht=scontent.flcj1-1.fna&oh=00_AT95xVHCXf4KwNoz6njV2hxclzsYK1FQftJZD3xPidiV6g&oe=62DB5A5B", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294643278_5459352970787944_7631255931786171849_n.jpg?stp=dst-jpg_p960x960&_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=_uxBg2Sqdy8AX8hbpFQ&tn=o5ey6nqbzOgQXIue&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-h-buTYqyErOXKdjFtjDMJARgaKFCPJH4H3Dw9A1i0BQ&oe=62DBED79",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294550007_5459352940787947_4058220204807437310_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=rxS9l66kFZkAX9ZshGm&tn=o5ey6nqbzOgQXIue&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8OiqFrcIbe2aUoomA3ObacdUYUpBY_4xNyUlee1btYxg&oe=62DC7865",
+    img1: baseURL+"rynekapartament11.webp",
+    img2: baseURL+"rynekapartament12.webp",
+    img3: baseURL+"rynekapartament13.webp", 
     plname: "Apartament na wynajem",
     pldescription:
       "Projekt, nadzór i realizacja",
@@ -203,10 +203,10 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294602156_5459359047454003_1936335882180417279_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=bJMZlxlSrmIAX_ISeO9&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_RJDmyw5JAs7p8-zKDuItnozN3CN3xlJA5hoqtQ-XIkA&oe=62DC1CA8", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294641457_5459359094120665_619825005820754924_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=6Ah0uQEnFR8AX9IZ0bs&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8JnxV5SMMbzozCs4Pbb2Jp20e2DBLRHQa1PzFR1kJyzw&oe=62DBEB4F",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294895115_5459359124120662_1880867441823686260_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=1DvCVwfDHsYAX_r-VKL&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_X-y9YPPefn9mU2SuSu8-yM2L1WkrUWoPiCcXn7bFBEQ&oe=62DB8CD7",
-    plname: "Apartament na wynajem",
+    img1: baseURL+"rynekapartament21.webp",
+    img2: baseURL+"rynekapartament22.webp",
+    img3: baseURL+"rynekapartament23.webp", 
+        plname: "Apartament na wynajem",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -236,10 +236,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294564844_5459361100787131_933767230967111225_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=2kEFSzpNG_cAX--8DFC&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-n9vIyhbd5ykIRYRZeUcYFN9DF51rbS062One7L3JkyA&oe=62DB3E59", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294573650_5459361054120469_5953520647872814102_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=5gdmcVP1XCMAX_2FOBl&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8RbUO5dSdFB77pJGu1sGd-o1Q2Sb7FOz_EKeOx7jcCdw&oe=62DBF3B9",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294523093_5459361057453802_6739628388891525776_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=LgARziwncfMAX8M3bw9&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9MP4Nd31X9R9uqmTdNANGhTePSbnZsFFlf9pEApLtZLQ&oe=62DB8132",
-    plname: "Apartament na wynajem",
+    img1: baseURL+"rynekapartament31.webp",
+    img2: baseURL+"rynekapartament32.webp",
+    img3: baseURL+"rynekapartament33.webp",     plname: "Apartament na wynajem",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -269,10 +268,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294586029_5459363674120207_812774086369197653_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=wkNGhR2BLXoAX-Z-WC1&_nc_ht=scontent.flcj1-1.fna&oh=00_AT8fyF-Q0TJit2y2jdyuaT2U3QXQnDVlYink0RaH7qNZAw&oe=62DC0A06", 
-    img2: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294549481_5459363740786867_980353449088123052_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=EFjcMQidR7UAX9bSx5O&_nc_ht=scontent.flcj1-1.fna&oh=00_AT9K3Lz0dwMHEZXguCxDYUuX66xI1GA_pVPvavXsovQSEg&oe=62DB4A70",
-    img3: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294676973_5459363757453532_5042724346773945148_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=Y_j9M87ZkwoAX82igkq&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-Q8gXd8G8P6gvgNl7vs1MzT60zsVLuGcTsOe8nTW-7PQ&oe=62DC2C15",
-    plname: "Apartament na wynajem",
+    img1: baseURL+"rynekapartament41.webp",
+    img2: baseURL+"rynekapartament42.webp",
+    img3: baseURL+"rynekapartament43.webp",     plname: "Apartament na wynajem",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -302,10 +300,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294577203_5459315434125031_7290459420850800235_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=5DSu7NTrhzcAX-FmpQO&_nc_ht=scontent.flcj1-1.fna&oh=00_AT-r6VhhHtKiXhEQiUGodzRyVLkBCrwjAhmagW4nwZhoug&oe=62DB4BDA", 
-    img2: "",
-    img3: "",
-    plname: "Restauracja ul. Kanonicza",
+    img1: baseURL+"kurkawodna.webp",
+    img2: baseURL+"kurkawodna.webp",
+    img3: baseURL+"kurkawodna.webp",     plname: "Restauracja ul. Kanonicza",
     pldescription:
       "Projekt, nadzór i realizacja",
     endescription: "",
@@ -335,9 +332,9 @@ const Lista = [
     ),
   },
   {
-    img1: "https://scontent.flcj1-1.fna.fbcdn.net/v/t39.30808-6/294569309_5459325034124071_8458763497818631675_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=k12spKD43WQAX8T1dza&_nc_ht=scontent.flcj1-1.fna&oh=00_AT_WugOcrjy6Ux2HpS4vqXkCuxtzma-qbDRG-dFhLwE-VQ&oe=62DAACA2", 
-    img2: "",
-    img3: "",
+    img1: baseURL+"szoberowksa.webp",
+    img2: baseURL+"szoberowksa.webp",
+    img3: baseURL+"szoberowksa.webp",     
     plname: "Restauracja Szoberowska",
     pldescription:
       "Projekt, nadzór i realizacja",
