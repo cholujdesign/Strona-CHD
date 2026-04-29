f29ec598d035a45c3d0faa45481eb9ae3d11688e
cholujdesign <79589369+cholujdesign@users.noreply.github.com>
2026-04-29 10:54:35 +0200

    cashe fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+      static: 180,
+    },
+  },
   async redirects() {
     return [
       { source: '/portfolio/sycamore', destination: '/portfolio/california-villa', permanent: true },
